e6e7f1507b5666d087cb113bcada2bab6a621b54
Md Limon Apu <mdlimonapu@Mac.fritz.box>
2026-05-24 11:59:22 +0200

    remove tagline from navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,15 @@
-import { NavLink, Routes, Route, useLocation } from 'react-router-dom'
+import { NavLink, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import University from './pages/University'
 import Profile from './pages/Profile'
 
 export default function App() {
-  const location = useLocation()
-  const titles = {
-    '/': 'Find your perfect university',
-    '/university': 'University results',
-    '/profile': 'Your profile',
-  }
-
   return (
     <div className="app-shell">
       <header className="topbar">
         <div className="brand">
           <div className="brand-icon">🎓</div>
-          <div>
-            <h1>StudyFinder</h1>
-            <p>{titles[location.pathname] || ''}</p>
-          </div>
+          <h1>StudyFinder</h1>
         </div>
         <nav className="nav-links">
           <NavLink to="/">Home</NavLink>
